fabb80ab47c5e9958b9089db107d8fcb4b84569b
david <alejandrobojaca1@gmail.com>
2023-12-07 16:16:45 -0500

    url-shortener attempt 2

--- url-shortener/server/index.js
+++ url-shortener/server/index.js
@@ -1,42 +1,70 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Url = require("./models/Url");
+const { object, string, url, date, InferType } = require("yup");
+const { nanoid } = require("nanoid");
 
-const app = express();
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+mongoose.connect("mongodb://localhost/url-shortener");
 
-mongoose.connect("mongodb://localhost/url_shortener");
-
-app.get("/", (req, res) => {
-  console.log("HERE");
-  const { shortenedUrl } = req.body;
-  const resp = Url.find({ shortenedUrl });
-
-  if (resp !== null) {
-    console.log({ resp, shortenedUrl });
-    res.redirect(resp.url);
-  }
-
-  res.json({
-    error: "Could not find url",
-  });
+const UrlSchema = mongoose.Schema({
+  url: String,
+  slug: String,
 });
 
-app.post("/", async (req, res) => {
-  //check url
+const Url = new mongoose.model("urls", UrlSchema);
+
+const morgan = require("morgan");
+
+const app = express();
+app.enable("trust proxy");
+
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(morgan("tiny"));
+
+const urlSchema = object({
+  url: string().trim().url().required(),
+  slug: string()
+    .trim()
+    .matches(/^[\w\-]+$/i),
+});
+
+app.post("/url", async (req, res) => {
+  console.log(req.body);
+  let { url, slug } = req.body;
+  console.log(url, slug);
   try {
-    const { url } = req.body;
-    new URL(url);
-    const newUrl = new Url({ url, shortenedUrl: "123" });
-    await newUrl.save();
-    res.redirect("/");
+    console.log(url, slug);
+    await urlSchema.validate({ url, slug });
+
+    if (slug === "") {
+      slug = nanoid(5);
+    }
+
+    const url = new Url({ url, slug });
+
+    res.json({
+      msg: "OK",
+      slug,
+      url,
+    });
   } catch (e) {
-    console.error(e);
+    console.log(e);
     res.json({
       error: e,
     });
   }
 });
 
-app.listen(3000, () => console.log("Express server listening on port 3000"));
+app.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+
+  const url = Url.find({ slug });
+
+  if (url !== null) {
+    res.redirect(url.url);
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- url-shortener/server/models/Url.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const UrlSchema = mongoose.Schema({
-  url: String,
-  shortenedUrl: String,
-});
-
-module.exports = new mongoose.model("urls", UrlSchema);
