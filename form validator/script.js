1d69951a684b21f6d43de4cdb2a14ae4f9b56958
david <alejandrobojaca1@gmail.com>
2024-01-26 23:59:02 -0500

    basic: registration form

--- form validator/script.js
+++ form validator/script.js
@@ -3,13 +3,46 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
+const labels = document.querySelectorAll(".password-label");
+const passwordMessage = document.querySelector(".password-message");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(
-    username.value,
-    email.value,
-    password.value,
-    confirmPassword.value
-  );
+  const pass = password.value;
+  const passConfirm = confirmPassword.value;
+
+  if (!username.value) {
+    alert("Username is required");
+    return;
+  }
+
+  if (!email.value) {
+    alert("Email is required");
+    return;
+  }
+
+  if (!password.value) {
+    alert("Password is required");
+    return;
+  }
+
+  if (!confirmPassword.value) {
+    alert("Confirm Password is required");
+    return;
+  }
+
+  if (pass !== passConfirm) {
+    console.log("Passwords do not match");
+    labels.forEach((label) => {
+      label.classList.add("label-error");
+    });
+    passwordMessage.style.display = "block";
+    password.value = "";
+    confirmPassword.value = "";
+  } else {
+    labels.forEach((label) => {
+      label.classList.remove("label-error");
+    });
+    passwordMessage.style.display = "none";
+  }
 });
