c79fa417136975e5316d9984f3128130ef332a85
david <alejandrobojaca1@gmail.com>
2024-06-05 19:45:59 -0500

    update

---
+++ python/static-sites/src/test_htmlnode.py
@@ -0,0 +1,20 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python/static-sites/src/textnode.py
@@ -0,0 +1,38 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, Text_node2):
+        return (self.text == Text_node2.text
+                and self.text_type == Text_node2.text_type
+                and self.url == Text_node2.url
+                )
+    
+    def __repr__(self):
+        return f"TextNode({self.text} {self.text_type} {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")    
